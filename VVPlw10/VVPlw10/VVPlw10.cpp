b93b2655df56ab69d0e9ba12e667b28c127426e2
Nikita <druzhbinn@gmail.com>
2021-10-04 21:16:12 +0300

    Update lab works

---
+++ VVPlw10/VVPlw10/VVPlw10.cpp
@@ -0,0 +1,132 @@
+﻿#include <iostream>
+
+void zadacha1()
+{
+    int A{ 0 }, B{-1};
+    std::cout << "A = " << A << " B = " << B << "\nA > 2 и B <=3 ?";
+    if ((A > 2) && (B <= 3))
+    {
+        std::cout << "\nИстина";
+    }
+    else
+    {
+        std::cout << "\nЛожь";
+    }
+}
+
+void zadacha2()
+{
+    int A{ 1 }, B{ 2 }, C{ 0 };
+    std::cout << "\n\nA = " << A << " B = " << B << " C = " << C << "\nA < B < C ?";
+    if ((A < B) && (B < C))
+    {
+        std::cout << "\nИстина";
+    }
+    else
+    {
+        std::cout << "\nЛожь";
+    }
+}
+
+void zadacha3()
+{
+    int N{22}, P;
+    P = N % 2;
+    std::cout << "\n\nЧисло " << N << " двузначное и чётное?";
+    if ((N >= 10) && (N <= 99) && (P == 0))
+    {
+        std::cout << "\nИстина";
+    }
+    else
+    {
+        std::cout << "\nЛожь";
+    }
+}
+
+void zadacha4()
+{
+    int N{ 111 }, S, D, E;
+    std::cout << "\n\nЧисло " << N << ". Цифры данного числа образуют возрастающую или убывающую последовательность?";
+    S = floor(N / 100);
+    D = floor((N % 100) / 10);
+    E = N % 10;
+    if (((S > D) && (D > E)) || ((S < D) && (D < E)))
+    {
+        std::cout << "\nИстина";
+    }
+    else
+    {
+        std::cout << "\nЛожь";
+    }
+}
+
+void zadacha5()
+{
+    int N{ 1221 }, T, S, D, E;
+    std::cout << "\n\nЧисло " << N << " читается одинаково слева направо и справа налево?";
+    T = floor(N / 1000);
+    S = floor((N % 1000) / 10);
+    D = floor((N % 1000) / 100);
+    E = N % 10;
+    if ((S = D) && (T = E))
+    {
+        std::cout << "\nИстина";
+    }
+    else
+    {
+        std::cout << "\nЛожь";
+    }
+}
+
+void zadacha6()
+{
+    int a{ 3 }, b{ 4 }, c{ 5 };
+    std::cout << "\n\nТреугольник со сторонами " << a << " " << b << " " << c << " " << " является прямоугольным?";
+    a = a * a;
+    b = b * b;
+    c = c * c;
+    bool f{ false };
+    if ((a == (b + c)) || (b == (a + c)) || (c == (a + b)))
+    {
+        f = true;
+    }
+    if (f == true)
+    {
+        std::cout << "\nИстина";
+    }
+    else
+    {
+        std::cout << "\nЛожь";
+    }
+}
+
+void zadacha7()
+{
+    int a{ 1 }, b{ 4 }, c{ 5 };
+    std::cout << "\n\nТреугольник со сторонами " << a << " " << b << " " << c << " " << " cуществует?";
+    bool f{ false };
+    if ((a + b > c) && (a + c > b) && (b + c > a))
+    {
+        f = true;
+    }
+    if(f == true)
+    {
+        std::cout << "\nИстина";
+    }
+    else
+    {
+        std::cout << "\nЛожь";
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    zadacha1();
+    zadacha2();
+    zadacha3();
+    zadacha4();
+    zadacha5();
+    zadacha6();
+    zadacha7();
+}
---
+++ VVPlw6/VVPlw6/VVPlw6.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+
+void zadacha1()
+{
+	double A{ 0 }, B{ 1 };
+	std::cout << "A = " << A << " B = " << B;
+	A = A + B;
+	B = -B + A;
+	A = A - B;
+	std::cout << "\nA = " << A << " B = " << B;
+}
+
+void zadacha2()
+{
+	double A{ 0 }, B{ 1 }, C{ 2 }, S;
+	std::cout << "\n\nA = " << A << " B = " << B << " C = " << C;
+	S = B;
+	B = A;
+	A = S; // A = B; B = A; C = C
+	S = C;
+	C = A;
+	A = S; // A = C; B = A; C = B
+	std::cout << "\nA = " << A << " B = " << B << " C = " << C;
+}
+
+void zadacha3()
+{
+	double A{ 0 }, B{ 1 }, C{ 2 }, S;
+	std::cout << "\n\nA = " << A << " B = " << B << " C = " << C;
+	S = C;
+	C = A;
+	A = S; // A = C; B = B; C = A
+	S = B;
+	B = A;
+	A = S; // A = C; B = C; C = B
+	std::cout << "\nA = " << A << " B = " << B << " C = " << C;
+}
+
+void zadacha4()
+{
+	double x{ 2 }, y{ -7 };
+	x = x * x; // x ^ 2
+	y = y - 6 * x;
+	x = x * x * x; // x ^ 6
+	y = y + 3 * x;
+	std::cout << "\n\ny = " << y;
+}
+
+void zadacha5()
+{
+	double x{ 5 }, y{ 2 }, P;
+	P = x - 3;
+	P = P * P * P; // (x-3) ^ 3
+	y = y - 7 * P;
+	P = P * P; // (x-3) ^ 6
+	y = y + 4 * P;
+	std::cout << "\n\ny = " << y;
+}
+
+void zadacha6()
+{
+	double A{ 2 }, P;
+	P = A * A; // A^2
+	P = P * P; // A^4
+	P = P * P; // A^8, вообще можно было бы обойтись и без вспомогательной переменной
+	A = P;
+	std::cout << "\n\nA^8 = " << A;
+}
+
+void zadacha7()
+{
+	double A{ 2 }, P1, P2;
+	P1 = A * A * A; // A^3
+	P2 = P1 * P1; // A^6
+	P2 = P2 * P2; // A^12
+	A = P1 * P2;
+	std::cout << "\n\nA^15 = " << A;
+}
+
+int main()
+{
+	zadacha1();
+	zadacha2();
+	zadacha3();
+	zadacha4();
+	zadacha5();
+	zadacha6();
+	zadacha7();
+	return 0;
+}
